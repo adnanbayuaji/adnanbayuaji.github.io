10df6656d5e856fe4eb049371ca5562aad87bdc9
adnanbayuaji <adnan.bayuaji@gmail.com>
2023-07-04 03:10:19 +0700

    add nav hamburger

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,8 @@ module.exports = {
     },
     extend: {
       colors:{
-        primary: '#14b8a6',
-        secondary: '#64748b',
+        primary: '#0A6EBD',
+        secondary: '#5A96E3',
         dark: '#0f172a',
       },
       screens:{
